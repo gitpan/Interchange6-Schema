ed13a5aafe49e55aca5f2c76f360530a127e45c1
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-11-15 09:35:19 +0000

    Import of HORNBURG/Interchange6-Schema-0.001 from CPAN.
    gitpan-cpan-distribution: Interchange6-Schema
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         HORNBURG/Interchange6-Schema-0.001.tar.gz
    gitpan-cpan-author:       HORNBURG
    gitpan-cpan-maturity:     released

---
+++ lib/Interchange6/Schema.pm
@@ -0,0 +1,54 @@
+use utf8;
+package Interchange6::Schema;
+
+=head1 NAME
+
+Interchange6::Schema - Database Schema for Interchange 6
+
+=head1 VERSION
+
+0.001
+
+=head1 CREATE SQL FILES FOR DATABASE SCHEMA
+
+This command creates SQL files for our database schema
+in the F<sql/> directory:
+
+    PERL5LIB=lib/ bin/interchange6-create-database
+
+=head1 AUTHORS
+
+Stefan Hornburg (Racke), C<racke@linuxia.de>
+Jeff Boes, C<jeff@endpoint.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Stefan Hornburg (Racke), Jeff Boes.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+our $VERSION = '0.001';
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-11-08 09:31:06
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:A+AhSjuWjRp6Y39vdVcJxg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
